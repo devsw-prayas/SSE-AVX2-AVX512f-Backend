2cf6aea5c1d9524cf637c1bc4481667625593693
DevSw <64429051+devsw-prayas@users.noreply.github.com>
2026-02-13 20:00:02 +0530

    Update v256Intrin.h

--- v256Intrin.h
+++ v256Intrin.h
@@ -1,3 +1,23 @@
+/*
+* Copyright (c) 2025 StormWeaver
+*
+* This file is part of the Standard Vector Backend
+*
+* Licensed under the MIT License. You may obtain a copy of the License at
+* https://opensource.org/licenses/MIT
+*
+* Permission is hereby granted, free of charge, to any person obtaining a copy
+* of this software and associated documentation files (the "Software"), to deal
+* in the Software without restriction, including without limitation the rights
+* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+* copies of the Software, and to permit persons to whom the Software is
+* furnished to do so, subject to the following conditions:
+*
+* The above copyright notice and this permission notice shall be included in all
+* copies or substantial portions of the Software.
+*
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND...
+*/
 #pragma once
 #define ALLOW_VECTOR_INTRIN_128
 #include <v128Intrin.h>
@@ -1873,4 +1893,5 @@ namespace _vectorbackendnamespace::Vectorization::Intrinsic::v256 {
 }
 #endif
 #endif
+
 
